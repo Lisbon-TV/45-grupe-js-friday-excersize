60990b90263bca005700b73dc3c962101e66db7f
Lisbon <lisbon@dublin.com>
2023-06-02 10:17:21 +0300

    exc 1

--- js/02-uzduotis.js
+++ js/02-uzduotis.js
@@ -1,15 +1,44 @@
 console.clear();
 
-/*
-Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus (Jonas Jonaitis). Atspausdinti trumpesnį stringą.
+/* Sukurti du kintamuosius. 
+Jiems priskirti savo mylimo aktoriaus vardą ir pavardę, kaip stringus (Jonas Jonaitis). 
+Atspausdinti trumpesnį stringą.
+*/
 
-Sukurkite 4 kintamuosius, kurie saugotų jūsų vardą, pavardę, gimimo metus ir šiuos metus (nebūtinai tikrus). Parašykite kodą, kuris pagal gimimo metus paskaičiuotų jūsų amžių ir naudodamas vardo ir pavardės kintamuosius atspausdintų tokį sakinį :"Aš esu Vardenis Pavardenis. Man yra XX metai(ų)".
+const vardas = "Tom";
+const pavarde = "Cruise";
+const pilnasVardas = `${vardas} ${pavarde}`;
+console.log(pilnasVardas)
 
-Sukurti du kintamuosius. Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus. Sukurti trečią kintamąjį ir jam priskirti stringą, sudarytą iš trijų paskutinių vardo ir pavardės kintamųjų raidžių. Jį atspausdinti.
 
-Sukurti kintamąjį su stringu: “Once upon a time in hollywood”. Jame visas “o” (didžiąsias ir mažąsias) pakeisti žvaigždutėm “*”.  Rezultatą atspausdinti.
+/* Sukurkite 4 kintamuosius, 
+kurie saugotų jūsų vardą, pavardę, 
+gimimo metus ir šiuos metus (nebūtinai tikrus). 
+Parašykite kodą, kuris pagal gimimo metus paskaičiuotų jūsų amžių 
+ir naudodamas vardo ir pavardės kintamuosius bei
+atspausdintų tokį sakinį :"Aš esu Vardenis Pavardenis. Man yra XX metai(ų)". 
+*/
 
-Sukurkite keturis kintamuosius kuriems sugeneruokite atsitiktines reikšmes nuo 0 iki 2. Suskaičiuokite kiek yra nulių, vienetų ir dvejetų.
 
+
+/* Sukurti du kintamuosius. 
+Jiems priskirti savo mylimo aktoriaus vardą ir pavardę kaip stringus. 
+Sukurti trečią kintamąjį ir jam priskirti stringą, 
+sudarytą iš trijų paskutinių vardo ir pavardės kintamųjų raidžių. 
+Jį atspausdinti.
+*/
+
+
+
+/* Sukurti kintamąjį su stringu: “Once upon a time in hollywood”. 
+Jame visas “o” (didžiąsias ir mažąsias) pakeisti žvaigždutėm “*”.  
+Rezultatą atspausdinti.
+*/
+
+
+
+/* Sukurkite keturis kintamuosius,
+kuriems sugeneruokite atsitiktines reikšmes nuo 0 iki 2. 
+Suskaičiuokite kiek yra nulių, vienetų ir dvejetų.
 */
 
